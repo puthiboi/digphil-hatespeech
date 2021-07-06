670b216eca51c42c7d89a8ebd7912ddfbe3aa55d
puthiboi <73469982+puthiboi@users.noreply.github.com>
2021-07-06 15:45:10 +0200

    Add existing file

---
+++ credentials.py
@@ -0,0 +1,6 @@
+####input your credentials here
+
+consumer_key = 'iUccfbzK0lbN4eHJu6V5Xwx8E'
+consumer_secret = '8rcQWEz0yQk5xYGeLQYIJcguDjzryKsOv7GPk9SJwI9fTz10hG'
+access_token = '1265951102958272513-k5HArrVJ1RSOkav3QWNokitvjgGdWP'
+access_token_secret = '6mZPc58u5FWBxaa5ueK2Wj9gRe13iQrP2kZML1MNYx87r'
--- twitterscrape.py
+++ twitterscrape.py
@@ -4,6 +4,7 @@ import csv
 import pandas as pd
 import time
 
+
 try:
     from credentials import consumer_key, consumer_secret, access_token, access_token_secret
 except ModuleNotFoundError:
@@ -16,23 +17,24 @@ auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
 auth.set_access_token(access_token, access_token_secret)
 api = tweepy.API(auth,wait_on_rate_limit=True)
 
-#####EuroSoccerChampionship
+#5 minutes from now to break for loop
+timeout = time.time() + 60*5
+
+#####EuroVisionSongContest
 # Open/Create a file to append data
 csvFile = open('eurosong2021.csv', 'a')
 #Use csv Writer
 csvWriter = csv.writer(csvFile)
 
-#5 minutes from now to break for loop
-timeout = time.time() + 60*5
-
-for tweet in tweepy.Cursor(api.search,q="eurovision",count=200,
+for tweet in tweepy.Cursor(api.search,q="eurovision",count=100,
                            lang="en",
-                           since="2021-06-11",
-                           until="2021-05-22").items():
+                           since="2021-06-11"
+                           ).items():
     print (tweet.created_at, tweet.text)
     csvWriter.writerow([tweet.created_at, tweet.text.encode('utf-8')])
     if time.time() > timeout:
         break
+
 
 
 
