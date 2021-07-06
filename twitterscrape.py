cd677de062ddef97f9d64dd6a4a11b1ee436ec8d
puthiboi <73469982+puthiboi@users.noreply.github.com>
2021-07-06 15:06:51 +0200

    Add existing file

--- twitterscrape.py
+++ twitterscrape.py
@@ -1,13 +1,16 @@
+import sys
 import tweepy
 import csv
 import pandas as pd
 import time
 
-####input your credentials here
-consumer_key = ''
-consumer_secret = ''
-access_token = ''
-access_token_secret = ''
+try:
+    from credentials import consumer_key, consumer_secret, access_token, access_token_secret
+except ModuleNotFoundError:
+    sys.exit('credentials.py does not exist in src/ folder. '
+             'See README.md for instructions.')
+except ImportError as import_error:
+    sys.exit(f'{import_error}\nCheck for spelling.')
 
 auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
 auth.set_access_token(access_token, access_token_secret)
@@ -15,24 +18,12 @@ api = tweepy.API(auth,wait_on_rate_limit=True)
 
 #####EuroSoccerChampionship
 # Open/Create a file to append data
-csvFile = open('eurosoc2021.csv', 'a')
-#Use csv Writer
-csvWriter = csv.writer(csvFile)
-
-for tweet in tweepy.Cursor(api.search,q="#euro2021",count=100,
-                           lang="en",
-                           since="2017-04-03").items():
-    print (tweet.created_at, tweet.text)
-    csvWriter.writerow([tweet.created_at, tweet.text.encode('utf-8')])
-
-#5 minutes from now to break for loop
-timeout = time.time() + 60*5
-
-#####EuroVisionSongContest
-# Open/Create a file to append data
 csvFile = open('eurosong2021.csv', 'a')
 #Use csv Writer
 csvWriter = csv.writer(csvFile)
+
+#5 minutes from now to break for loop
+timeout = time.time() + 60*5
 
 for tweet in tweepy.Cursor(api.search,q="eurovision",count=200,
                            lang="en",
@@ -43,3 +34,6 @@ for tweet in tweepy.Cursor(api.search,q="eurovision",count=200,
     if time.time() > timeout:
         break
 
+
+
+    
