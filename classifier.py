7515ecd3d2596904766dc79cb45a098f1e32b966
puthiboi <73469982+puthiboi@users.noreply.github.com>
2021-08-21 09:05:53 +0200

    Update classifier.py and predict.py

--- classifier.py
+++ classifier.py
@@ -1,6 +1,7 @@
 import pandas as pd
 import numpy as np
 import time
+import sys
 #import matplotlib.pyplot as plt
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.ensemble import AdaBoostClassifier
@@ -12,10 +13,10 @@ from sklearn.feature_extraction.text import TfidfTransformer
 
 cores = 3 # how many cpu cores one wants to use (for most classifiers)
 
-randforclf = False ##use the random forest classifier in this run?
+randforclf = True ##use the random forest classifier in this run?
 no_of_trees = 240 #number of trees in the forest of the random forest classifier
 
-adaboost = True #use adaboost in this run?
+adaboost = False #use adaboost in this run?
 no_est_ada = 100 #number of weak estimators used for adaboost
 
 
@@ -39,14 +40,6 @@ tfidf_transformer = TfidfTransformer()
 tweets_tfidf = tfidf_transformer.fit_transform(X_train_counts)
 print (tweets_tfidf.shape)
 
-
-### split the data
-print("Split the Data")
-tweets_tfidf_train, tweets_tfidf_test, hate_speech_train, hate_speech_test = train_test_split(
-    tweets_tfidf, hate_speech, 
-    test_size=0.20, random_state=42)
-
-
 ### train the classifier using the training data
 
 
@@ -55,23 +48,35 @@ if randforclf == True:
     print(f"Training Random Forest Classifier with {no_of_trees} estimators")
     start = time.time()
     clf = RandomForestClassifier(n_estimators=no_of_trees, n_jobs=cores)
-    clf.fit(tweets_tfidf_train, hate_speech_train)
+    clf.fit(tweets_tfidf, hate_speech)
 
-    ### compute accuracy using test data
-    acc_test = clf.score(tweets_tfidf_test, hate_speech_test)
-    print ("Test Accuracy:", acc_test)
     end = time.time()
     print ("Training Time: ", end - start)
+    
 
 
 if adaboost == True:
     start = time.time()
     print(f"training AdaBoost with {no_est_ada} estimators")
     clf = AdaBoostClassifier(n_estimators=no_est_ada)  # , n_jobs=cores)
-    clf.fit(tweets_tfidf_train, hate_speech_train)
+    clf.fit(tweets_tfidf, hate_speech)
 
-    ### compute accuracy using test data
-    acc_test = clf.score(tweets_tfidf_test, hate_speech_test)
-    print("Test Accuracy:", acc_test)
     end = time.time()
     print ("Training Time: ", end - start)
+    
+start2 = time.time()    
+try:
+    from predict import tweets_tfidf_song, tweets_tfidf_soc
+except ModuleNotFoundError:
+    sys.exit('predict.py import sysdoes not exist in src/ folder. '
+             )
+except ImportError as import_error:
+    sys.exit(f'{import_error}\nCheck for spelling.')
+
+hate_speech_new_soc = clf.predict(tweets_tfidf_soc)
+
+end2 = time.time()
+print ("Prediction Time: ", end2 - start2)
+
+#for i in range(len(tweets_tfidf_soc)):
+#    print("X=%s, Predicted=%s" % (tweets_tfidf_soc[i], hate_speech_new_soc[i]))
--- predict.py
+++ predict.py
@@ -1,9 +1,5 @@
 import pandas as pd
 import numpy as np
-import time
-#import matplotlib.pyplot as plt
-from sklearn.ensemble import RandomForestClassifier
-from sklearn.ensemble import AdaBoostClassifier
 from sklearn.model_selection import train_test_split
 from sklearn.feature_extraction.text import CountVectorizer
 from sklearn.feature_extraction.text import TfidfTransformer
@@ -11,9 +7,16 @@ from sklearn.feature_extraction.text import TfidfTransformer
 
 ### Load & Filter dataset
 print("Load Dataset & Filter")
+
+###eurosoc
 df_soc = pd.read_csv('eurosoc.csv',
                  lineterminator='\n')
 df_soc_selected = df_soc.drop(['Datetime','Username'], axis=1)
+
+###eurosong
+df_song = pd.read_csv('eurosong.csv',
+                 lineterminator='\n')
+df_song_selected = df_song.drop(['Datetime','Username'], axis=1)
 
 
 
@@ -21,11 +24,27 @@ df_soc_selected = df_soc.drop(['Datetime','Username'], axis=1)
 print("Convert to Array")
 text_soc = np.asarray(df_soc_selected.Text)
 
+text_song = np.asarray(df_song_selected.Text)
+
+
 print("Countvec")
 count_vect_soc = CountVectorizer()
 X_predict_counts_soc = count_vect_soc.fit_transform(text_soc)
 
+count_vect_song = CountVectorizer()
+X_predict_counts_song = count_vect_song.fit_transform(text_song)
+
 print("TfidfTrans")
-tfidf_transformer = TfidfTransformer()
-tweets_tfidf_soc = tfidf_transformer.fit_transform(X_predict_counts_soc)
-print (tweets_tfidf_soc.shape)
+tfidf_transformer_soc = TfidfTransformer()
+tweets_tfidf_soc = tfidf_transformer_soc.fit_transform(X_predict_counts_soc)
+
+tfidf_transformer_song = TfidfTransformer()
+tweets_tfidf_song = tfidf_transformer_song.fit_transform(X_predict_counts_song)
+
+print(tweets_tfidf_soc)
+print(tweets_tfidf_song.shape)
+
+
+
+
+
